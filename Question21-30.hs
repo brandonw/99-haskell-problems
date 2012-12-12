0500bfa1351bc708dce3478d92f0cce4a53d81f1
Brandon Waskiewicz <brandon.waskiewicz@gmail.com>
2012-12-11 22:58:31 -0500

    Finished a few more questions

--- Question21-30.hs
+++ Question21-30.hs
@@ -16,11 +16,37 @@ range x y = range' x y []
         | x < y = range' x (y - 1) (y : zs)
         | x > y = range' x (y + 1) (y : zs)
 
-rndSelect :: [a] -> Int -> [a]
-rndSelect _ 0   = []
-rndSelect [] _  = []
+rndSelect :: [a] -> Int -> IO [a]
+rndSelect _ 0   = return []
+rndSelect [] _  = return []
 rndSelect xs n
-    | n < 0     = []
+    | n < 0     = return []
     | otherwise = do
         g <- getStdGen
         return $ take n $ map (xs !!) $ randomRs (0, length xs - 1) g
+
+rndSelect' :: [a] -> Int -> IO [a]
+rndSelect' _ 0  = return []
+rndSelect' [] _ = return []
+rndSelect' xs n
+    | n < 0     = return []
+    | otherwise =  rndSelect'' xs n []
+      where
+        rndSelect'' _ 0 rs  = return rs
+        rndSelect'' xs' c rs = do
+            r <- randomRIO (0, length xs' - 1)
+            rndSelect'' (take r xs' ++ drop (r + 1) xs') (c - 1) $ (xs' !! r) : rs
+
+diffSelect :: Int -> Int -> IO [Int]
+diffSelect 0 _   = return []
+diffSelect n max = rndSelect' [1..max] n
+
+rndPermu :: [a] -> IO [a]
+rndPermu []  = return []
+rndPermu [x] = return [x]
+rndPermu xs  = rndPermu' xs []
+  where
+    rndPermu' [] rs  = return rs
+    rndPermu' xs' rs = do
+        r <- randomRIO (0, length xs' - 1)
+        rndPermu' (take r xs' ++ drop (r + 1) xs') $ xs' !! r : rs
